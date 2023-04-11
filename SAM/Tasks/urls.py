adf3fc850f45498219ee066f843e5c1b3c8897d0
notmateli <samcourtois02@gmail.com>
2023-04-11 13:19:29 +0300

    4/11/20923

--- SAM/Tasks/urls.py
+++ SAM/Tasks/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .import views
 
+app_name = "tasks"
 
 urlpatterns = [
-    path("", views.task, name="task"),
+    path("", views.test, name="test"),
+    path("add/", views.add, name="add"),
 
 ]
--- SAM/Tasks/views.py
+++ SAM/Tasks/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 tasks = ["Buy groceries", "Play games", "Watch Football"]
-def task(request):
+def test(request):
     return render(request, "tasks.html", {
 
-        "tasks": tasks
+    "Tasks": tasks
 
     })
+def add(request):
+    return render(request, "add.html")
