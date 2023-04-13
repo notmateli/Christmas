a8716bd4e93a623aa076fefd4222a74acfc35732
notmateli <samcourtois02@gmail.com>
2023-04-13 12:51:53 +0300

    13/4/2023

--- SAM/Tasks/views.py
+++ SAM/Tasks/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django import forms
 # Create your views here.
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Task Field")
+    phone = forms.IntegerField(label="Age", min_value="10", max_value="12")
+
+class OldTaskForm(forms.Form):
+    task = forms.CharField(label="Email")
 
 tasks = ["Buy groceries", "Play games", "Watch Football"]
 def test(request):
@@ -10,4 +16,10 @@ def test(request):
 
     })
 def add(request):
-    return render(request, "add.html")
+    return render(request, "add.html",{
+        "form": NewTaskForm()
+    })
+def ongeza(request):
+    return render(request, "ongeza.html", {
+        "form": OldTaskForm()
+    })
