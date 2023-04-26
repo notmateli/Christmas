90b0ca7b41ab95ed5f72a2d25f3b18889cbe35e3
notmateli <samcourtois02@gmail.com>
2023-04-26 13:07:37 +0300

    pushed again

--- SAM/crud/urls.py
+++ SAM/crud/urls.py
@@ -5,6 +5,6 @@ from .import views
 urlpatterns = [
     path("", views.crud, name="crud"),
     path("insert", views.insertData, name="insertData"),
-    path('update/<id>', views.updateData, name="updateData"),
-    path('delete/<id>', views.deleteData, name="deleteData"),
+    path("update/<id>", views.updateData, name="updateData"),
+    path("delete/<id>", views.deleteData, name="deleteData"),
 ]
